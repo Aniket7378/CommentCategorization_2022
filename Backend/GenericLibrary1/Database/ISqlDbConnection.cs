67bffbcd27565c46b1746f89bb88afe300288fd8
Shaswat Shukla <aniketingh7378@gmail.com>
2022-07-21 15:05:17 +0530

    commit

--- Backend/GenericLibrary1/Database/ISqlDbConnection.cs
+++ Backend/GenericLibrary1/Database/ISqlDbConnection.cs
@@ -9,10 +9,18 @@ namespace GenericLibrary.Database
 {
     public interface ISqlDbConnection
     {
+        /// <summary>
+        /// Connecting to database using connection string.
+        /// </summary>
         SqlConnection SqlConnectionToDb { get; set; }
 
+        /// <summary>
+        /// Opens the database connection if closed. 
+        /// </summary>
         void Open();
-
+        /// <summary>
+        /// Closes the database connection if opened.
+        /// </summary>
         void Close();
     }
 }
